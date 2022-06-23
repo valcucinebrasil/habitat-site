063968ff5d9af03b23b9ff493fe2fac4953131e2
valcucinebrasil <102249283+valcucinebrasil@users.noreply.github.com>
2022-06-23 13:53:31 -0300

    upgrade

--- assets/JS/email.js
+++ assets/JS/email.js
@@ -4,19 +4,20 @@ btn.addEventListener('click', () => {
 
     const name = (inputs.elements["name"].value).trim()
     const email = (inputs.elements["email"].value).trim()
-    const msg = (inputs.elements["message"].value.trim())
+    const select = (inputs.elements["select"].value).trim()
+    const msg = (inputs.elements["message"].value).trim()
     const phone = (inputs.elements["phone"].value).trim()
     if (!name.length > 0 || !email.length > 0 || !msg.length > 0 || !phone.length > 0) {
         alert("Esse campo é obrigatório")
         return
     }
-    email.send({
-        Host: "smtp.mailtrap.io",
-        Username: "fe398f530b3dee",
-        Password: "9ae44c79299335",
+    Email.send({
+        Host: "email-ssl.com.br",
+        Username: "projetos3@valcucinebrasil.com.br",
+        Password: "Zoe-2022",
         To: "projetos4@valcucinebrasil.com.br",
         From: email,
         Subject: "Show, você tem um novo orçamento",
-        Body: msg + "<br>" + name + "<br>" + phone
-    }).then(msg => alert("Sua mensagem foi enviada com sucesso!")
+        Body: msg + "<br>" + name + "<br>" + phone + "<br>" + select
+    }).then(msg => alert("Sua mensagem foi enviada com sucesso!"))
 })
